e5b2abc52d5effcbea225fc7df99275fa9e0681e
Elmer Antonio Angel Reyes <elmerangel2800@gmail.com>
2020-08-04 17:48:47 -0500

    Agregar archivos de proyecto.

---
+++ Programacion 1/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        'Dim es una palabra reservada para declara una variable
+        Dim num1, num2, respuesta As Double
+        num1 = txtnum1.Text
+        num2 = txtnum2.Text
+        If optSuma.Checked Then
+            lblrespuesta.Text = num1 + num2
+        End If
+
+        If optResta.Checked Then
+            lblrespuesta.Text = num1 - num2
+        End If
+
+        If optMultiplicacion.Checked Then
+            lblrespuesta.Text = num1 * num2
+        End If
+
+        If optDivision.Checked Then
+            lblrespuesta.Text = num1 / num2
+        End If
+
+        If optPortsentaje.Checked Then
+            lblrespuesta.Text = num1 * num2 / 100
+        End If
+
+        If optExponenciacion.Checked Then
+            lblrespuesta.Text = num1 ^ num2
+        End If
+
+        If optMod.Checked Then
+            lblrespuesta.Text = num1 Mod num2
+        End If
+    End Sub
+End Class
---
+++ Programacion 1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Programacion 1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("Programacion 1")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("6842145c-d8b6-4dd8-927e-d93bebb6db0f")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
