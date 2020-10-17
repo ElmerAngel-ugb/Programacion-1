4f9c8c2d0a985c180f6f8f8b9fc23a830caca841
Flor Mabel Contreras Rodríguez <usis011120@ugb.edu.sv>
2020-10-17 12:38:17 -0600

    Sincronizado

---
+++ Programacion 1/dbconexion.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class dbconexion
+    Dim miConexion As New SqlConnection
+    Dim miCommand As New SqlCommand
+    Dim miAdapter As New SqlDataAdapter
+    Dim ds As DataSet
+    Public Sub db_conexion()
+        Dim cadenaConeccion As String
+        cadenaConeccion = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\sistemaclinicodb.mdf;Integrated Security=True"
+        miConexion.ConnectionString = cadenaConeccion
+
+        miConexion.Open()
+
+    End Sub
+    Public Function o()
+
+End Class
