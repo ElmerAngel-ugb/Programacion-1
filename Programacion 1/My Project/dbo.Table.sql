620e98d719dc4acee1a23c49779f340d95d4baba
Flor Mabel Contreras Rodríguez <usis011120@ugb.edu.sv>
2020-10-17 10:10:26 -0600

    Se agrego la tabla registro de personal a la base de datos

---
+++ Programacion 1/My Project/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[registrodepersonal]
+(
+	[Idregistrodepersonal] INT NOT NULL PRIMARY KEY IDENTITY(0, 1), 
+    [codigo] CHAR(10) NOT NULL, 
+    [nombre] CHAR(65) NOT NULL, 
+    [direccion] CHAR(100) NOT NULL, 
+    [telefono ] CHAR(10) NOT NULL, 
+    [email] CHAR(65) NULL, 
+    [cargo] CHAR(35) NOT NULL, 
+    [horario] CHAR(70) NOT NULL
+)
