280e95ea535f3482ed68b153b71c64b29c50bce2
Flor Mabel Contreras Rodríguez <usis011120@ugb.edu.sv>
2020-10-17 14:25:28 -0600

    base de datos

--- Programacion 1/FmrRegistrodePersonalvb.Designer.vb
+++ Programacion 1/FmrRegistrodePersonalvb.Designer.vb
@@ -336,7 +336,7 @@ Partial Class FmrRegistrodePersonalvb
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(517, 452)
+        Me.ClientSize = New System.Drawing.Size(520, 447)
         Me.Controls.Add(Me.grbEditarRegistrodePersonal)
         Me.Controls.Add(Me.grbRNavegarRegistrodePersonal)
         Me.Controls.Add(Me.grbRegistroPersonal)
