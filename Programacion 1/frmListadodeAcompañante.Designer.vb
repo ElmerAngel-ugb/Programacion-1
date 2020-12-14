021cbfc78c4f1e30cc1ac0fa97ab129e0bc1545a
Flor Mabel Contreras Rodríguez <usis011120@ugb.edu.sv>
2020-12-13 18:42:33 -0600

    Nuevo informe

---
+++ Programacion 1/frmListadodeAcompañante.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmListadodeAcompañante
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.sistemaclinicodbDataSet = New Programacion_1.sistemaclinicodbDataSet()
+        Me.AcompananteBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.AcompananteTableAdapter = New Programacion_1.sistemaclinicodbDataSetTableAdapters.AcompananteTableAdapter()
+        CType(Me.sistemaclinicodbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AcompananteBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        ReportDataSource2.Name = "dslistadodeAcompañante"
+        ReportDataSource2.Value = Me.AcompananteBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "Programacion_1.ifmListadodeAcompañantes.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 12)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(826, 533)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'sistemaclinicodbDataSet
+        '
+        Me.sistemaclinicodbDataSet.DataSetName = "sistemaclinicodbDataSet"
+        Me.sistemaclinicodbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'AcompananteBindingSource
+        '
+        Me.AcompananteBindingSource.DataMember = "Acompanante"
+        Me.AcompananteBindingSource.DataSource = Me.sistemaclinicodbDataSet
+        '
+        'AcompananteTableAdapter
+        '
+        Me.AcompananteTableAdapter.ClearBeforeFill = True
+        '
+        'frmListadodeAcompañante
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(850, 557)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "frmListadodeAcompañante"
+        Me.Text = "Listado de Acompañante"
+        CType(Me.sistemaclinicodbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AcompananteBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents AcompananteBindingSource As BindingSource
+    Friend WithEvents sistemaclinicodbDataSet As sistemaclinicodbDataSet
+    Friend WithEvents AcompananteTableAdapter As sistemaclinicodbDataSetTableAdapters.AcompananteTableAdapter
+End Class
---
+++ Programacion 1/frmListadodeAcompañante.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmListadodeAcompañante
+    Private Sub frmListadodeAcompañante_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'sistemaclinicodbDataSet.Acompanante' Puede moverla o quitarla según sea necesario.
+        Me.AcompananteTableAdapter.Fill(Me.sistemaclinicodbDataSet.Acompanante)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
